6393c76a9577fc64d5a48ca90bc266af8081c998
Selast Lambou <selastlambou@gmail.com>
2022-08-12 01:01:26 +0200

    Event updated with EventType and TontineId

--- common-event-model/src/main/java/com/djaller/common/event/model/AppEvent.java
+++ common-event-model/src/main/java/com/djaller/common/event/model/AppEvent.java
@@ -5,11 +5,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class AppEvent implements Serializable {
-    private String type;
+    private String eventType;
+    private UUID tontineId;
     private Object payload;
 }
