462f028af8850fcd399228c6f526dd3944b2eafc
Selast Lambou <selastlambou@gmail.com>
2022-08-13 23:35:03 +0200

    Add code-server to api-gateway config

--- gateway-server/src/main/java/com/djaller/server/auth/config/SecurityConfig.java
+++ gateway-server/src/main/java/com/djaller/server/auth/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.djaller.server.config;
+package com.djaller.server.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
@@ -13,11 +13,12 @@ public class SecurityConfig {
         http
                 .authorizeExchange(authorizeExchangeSpec ->
                         authorizeExchangeSpec
-                                .pathMatchers("/auth", "/oauth2/**", "/actuator/**").permitAll()
+                                .pathMatchers("/auth/**", "/oauth2/**", "/actuator/**").permitAll()
                                 .pathMatchers("/v1/m/api", "/v1/t/api", "/v1/w/api").permitAll()
                                 .anyExchange().authenticated()
                 )
-                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt)
+                .csrf(c -> c.disable());
         return http.build();
     }
 
